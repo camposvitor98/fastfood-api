53981dc5a4cc6bc745440745966fc44781f4d529
Vitor Campos <campos.vitor98@gmail.com>
2022-04-02 21:26:57 -0300

    test: get all ingredients

---
+++ src/modules/Ingredients/tests.spec.ts
@@ -0,0 +1,28 @@
+import { IngredientsCategory } from "@prisma/client";
+import { IngredientsServices } from "./services";
+
+describe("Get all ingredients", () => {
+  it("should return a list with all ingredients", async () => {
+    const ingredientsServices = new IngredientsServices();
+    const ingredients = await ingredientsServices.getAllIngredients();
+
+    expect(Array.isArray(ingredients)).toBe(true);
+
+    if (ingredients.length) {
+      const ingredient = ingredients[0];
+      expect(ingredient).toHaveProperty("id");
+      expect(ingredient).toHaveProperty("createdAt");
+      expect(ingredient).toHaveProperty("updatedAt");
+      expect(ingredient).toHaveProperty("name");
+      expect(ingredient).toHaveProperty("quantity");
+      expect(ingredient).toHaveProperty("ingredientCategory");
+
+      const isValidCategory = Object.keys(IngredientsCategory).includes(
+        ingredient.ingredientCategory
+      );
+      expect(isValidCategory).toBeTruthy();
+    } else {
+      expect(ingredients.length).toBeFalsy();
+    }
+  });
+});
